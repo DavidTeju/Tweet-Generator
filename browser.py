78c3eacd37820c9e5dd3aa26772f8da346622a63
David Tejuosho <tejuoshodavid@gmail.com>
2022-10-10 15:23:26 -0400

    update web driver to headless

--- browser.py
+++ browser.py
@@ -2,17 +2,26 @@ import os
 import time
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+driver_path = ChromeDriverManager().install()
+
 
 def get_pin(url):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = Options()
+    options.headless = True
+    # options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
+    # TODO: add this to ReadMe
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(service=Service(driver_path), options=options)
 
     driver.get(url)
 
-    driver.maximize_window()
     time.sleep(5)
 
     driver.find_element(By.ID, "username_or_email").send_keys(os.environ["TWITTER_USERNAME"])
