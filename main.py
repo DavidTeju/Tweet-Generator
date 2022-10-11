d1e259bc4ba40b21feb6ae5a1972b274dc7ce421
David Tejuosho <tejuoshodavid@gmail.com>
2022-10-11 02:35:33 -0400

    added safe-exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import signal
 import time
-from itertools import count
 
 import schedule as schedule
 from random_word import RandomWords
@@ -7,18 +7,24 @@ from random_word import RandomWords
 from NgramModel import NgramModel
 from inspector import tweet_to_sentences
 from twitter import *
-from tweet import tweet
 
 randomizer = RandomWords()
 bot = Twitter(os.environ["TWITTER_API_BEARER_TOKEN"],
               os.environ["TWITTER_API_KEY"],
               os.environ["TWITTER_API_KEY_SECRET"])
-first_model = NgramModel.load_existing_model("main-model")
+my_model = NgramModel.load_existing_model("main-model")
+"""to use a new model instead, use the NgramModel constructor"""
 
 
-def train_model(model: NgramModel):
+def query_and_train(model: NgramModel, client):
+    """
+    calls the methods to query the Twitter API and train the model
+
+    :param model: model to train
+    :param client: Twitter object to handle twitter processes
+    """
     query = randomizer.get_random_word()
-    for each_tweet in bot.get_tweets(query):
+    for each_tweet in client.get_tweets(query):
         model.train(tweet_to_sentences(each_tweet))
 
 
@@ -32,9 +38,17 @@ def exit_gracefully(*args):
 
 
 if __name__ == "__main__":
-    schedule.every(3).seconds.do(lambda: train_model(first_model))
-    schedule.every(3).hours.do(lambda: tweet(first_model, bot))
+    # I scheduled the training sequence to run every 3 seconds and the tweet to run every 3 hours
+    schedule.every(3).seconds.do(lambda: query_and_train(my_model, bot))
+    schedule.every(3).hours.do(lambda: generate_and_post_tweet(my_model, bot))
 
-    while True:
+    signal.signal(signal.SIGINT, exit_gracefully)
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+    exit_now = False
+    while not exit_now:
         schedule.run_pending()
         time.sleep(1)
+
+    my_model.backup()
+    print("Processes successfully stopped")
