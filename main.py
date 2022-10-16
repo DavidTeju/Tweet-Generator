a539838f3c5bc5a26b6b4fa337ead8e50c01c96b
David Tejuosho <tejuoshodavid@gmail.com>
2022-10-16 12:17:18 -0500

    gc test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import gc
 import signal
 import time
 
@@ -13,20 +14,29 @@ randomizer = RandomWords()
 bot = Twitter(os.environ["TWITTER_API_BEARER_TOKEN"],
               os.environ["TWITTER_API_KEY"],
               os.environ["TWITTER_API_KEY_SECRET"])
-my_model = NgramModel.load_existing_model("main-model")
+loader = lambda: NgramModel.load_existing_model("main-model")
+my_model = None
 """to use a new model instead, use the NgramModel constructor"""
 
 
-def query_and_train(model: NgramModel, client):
+def query_and_train(client):
     """
     calls the methods to query the Twitter API and train the model
 
-    :param model: model to train
     :param client: Twitter object to handle twitter processes
     """
-    query = randomizer.get_random_word()
-    for each_tweet in client.get_tweets(query):
-        model.train(tweet_to_sentences(each_tweet))
+    global my_model
+    my_model = loader()
+    for _ in range(200):
+        if exit_now: break
+        print("here")
+        query = randomizer.get_random_word()
+        for each_tweet in client.get_tweets(query):
+            my_model.train(tweet_to_sentences(each_tweet))
+    my_model.backup()
+    my_model = None
+    del my_model
+    gc.collect()
 
 
 def generate_and_post_tweet(model, api_client):
@@ -43,12 +53,11 @@ def exit_gracefully(*args):
 def run_server():
     global exit_now
     # I scheduled the training sequence to run every 3 seconds and the tweet to run every 3 hours
-    schedule.every(3).seconds.do(lambda: query_and_train(my_model, bot))
-    schedule.every(3).hours.do(lambda: generate_and_post_tweet(my_model, bot))
-    schedule.every(10).minutes.do(my_model.backup)
+    schedule.every().hours.do(lambda: query_and_train(bot))
+    # schedule.every(3).hours.do(lambda: generate_and_post_tweet(my_model, bot))
+    # schedule.every(10).minutes.do(my_model.backup)
 
     signal.signal(signal.SIGINT, exit_gracefully)
-    signal.signal(signal.SIGTERM, exit_gracefully)
 
     for i in signal.valid_signals():
         if (i == signal.SIGKILL or
@@ -59,13 +68,13 @@ def run_server():
     while not exit_now:
         schedule.run_pending()
         time.sleep(1)
-    my_model.backup()
     print("Processes successfully stopped")
 
 
 if __name__ == "__main__":
     exit_now = False
     """This is simply a way for me to make this a background process so it isn't closed on the server"""
-    with open("log", "w+") as file:
-        with DaemonContext(stderr=file, stdout=file):
-            run_server()
+    run_server()
+    # with open("log", "w+") as file:
+    #     with DaemonContext(stderr=file, stdout=file):
+    #         run_server()
